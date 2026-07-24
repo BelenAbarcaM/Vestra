eed53172e72c78ed906306cd110f3fa65c9d3403
BelenAbarcaM <belenabarcamonge@gmail.com>
2026-07-24 13:16:02 -0600

    Update editar.php

---
+++ vestra/backend/api/publicaciones/editar.php
@@ -0,0 +1,49 @@
+<?php
+
+session_start();
+include '../../config/conexion.php';
+
+$idPubli = $_POST['id_publicacion'];
+$texto = $_POST['texto'];
+$idClub = $_POST['id_club'];
+$idUsuario = $_SESSION['id_usuario'];
+$imagen = $_FILES['imagen'];
+
+if ($_FILES['imagen']['error'] == 0) {
+    $imagen = $_FILES['imagen'];
+    $nombreImagen = uniqid() . "_" . $imagen['name'];
+    $rutaDestino = __DIR__ . "/../../../uploads/publicaciones/" . $nombreImagen;
+
+    move_uploaded_file($imagen['tmp_name'], $rutaDestino);
+} else {
+    $sqlFoto = "SELECT Imagen_url
+                FROM Publicacion
+                WHERE id_publicacion = ?";
+
+    $stmtFoto = $conexion->prepare($sqlFoto);
+    $stmtFoto->bind_param("i", $idPublicacion);
+    $stmtFoto->execute();
+
+    $resultado = $stmtFoto->get_result();
+    $publicacion = $resultado->fetch_assoc();
+
+    $nombreImagen = $publicacion['Imagen_url'];
+}
+
+$edit = "UPDATE publicacion
+SET
+    Texto = ?,
+    id_usuario = ?,
+    id_club = ?,
+    Imagen_url = ?
+WHERE id_publicacion = ?";
+
+$stmt = $conexion->prepare($edit);
+$stmt->bind_param("siis", $texto, $idUsuario, $idClub, $nombreImagen, $idPublicacion);
+
+if ($stmt->execute()) {
+    echo "Publicación actualizada correctamente.";
+} else {
+    echo "Error: " . $stmt->error;
+}
+?>
