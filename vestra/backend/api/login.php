283395feeed3954a8292a6b855583434f51af841
BelenAbarcaM <belenabarcamonge@gmail.com>
2026-07-29 18:23:42 -0600

    Update login.php

--- vestra/backend/api/login.php
+++ vestra/backend/api/login.php
@@ -1,45 +1,56 @@
 <?php
 include '../config/conexion.php';
 
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
-$correo = $_POST['email'];
-$contra = $_POST['pass'];
+header("Content-Type: application/json");
 
-$consulta = mysqli_query($conexion, "SELECT * FROM usuario WHERE Correo='$correo'");
+session_start();
 
-if(mysqli_num_rows($consulta) > 0){
+$correo = $_POST['email'] ?? '';
+$contra = $_POST['pass'] ?? '';
 
-    $usuario = mysqli_fetch_assoc($consulta);
+$consulta = mysqli_prepare($conexion,
+"SELECT * FROM usuario WHERE Correo=?");
+
+mysqli_stmt_bind_param($consulta, "s", $correo);
+mysqli_stmt_execute($consulta);
+
+$resultado = mysqli_stmt_get_result($consulta);
+
+if(mysqli_num_rows($resultado) > 0){
+
+    $usuario = mysqli_fetch_assoc($resultado);
 
     if(password_verify($contra, $usuario['Contraseña'])){
 
-    session_start();
+        $_SESSION['id_usuario'] = $usuario['id_usuario'];
+        $_SESSION['usuario'] = $usuario['Nombre'];
+        $_SESSION['tipo'] = $usuario['id_tipo_usuario'];
 
-    $_SESSION['id_usuario'] = $usuario['id_usuario'];
-    $_SESSION['usuario'] = $usuario['Nombre'];
-    $_SESSION['tipo'] = $usuario['id_tipo_usuario'];
+        echo json_encode([
+            "success" => true,
+            "mensaje" => "Login correcto",
+            "usuario" => $usuario["Nombre"],
+            "tipo" => $usuario["id_tipo_usuario"]
+        ]);
 
+    }else{
 
-        header("location: ../bienvenida.php");
-        exit();
+        echo json_encode([
+            "success" => false,
+            "mensaje" => "Contraseña incorrecta"
+        ]);
 
-    } else {
-
-        echo '
-        <script>
-        alert("Contraseña incorrecta");
-        window.location = "../prueba.php";
-        </script>';
     }
 
 }else{
 
-    echo '
-    <script>
-    alert("Este usuario aún no existe");
-    window.location = "../prueba.php";
-    </script>';
+    echo json_encode([
+        "success" => false,
+        "mensaje" => "Usuario no encontrado"
+    ]);
 
 }
-
-?>
