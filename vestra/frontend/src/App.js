a62be6da1a69a2d9fdbb84d94afe31aa0c275211
BelenAbarcaM <belenabarcamonge@gmail.com>
2026-08-09 15:50:38 -0600

    Add files via upload

--- vestra/frontend/src/App.js
+++ vestra/frontend/src/App.js
@@ -1,14 +1,50 @@
+import { useState } from 'react';
 import './App.css';
-import Inicio from './componentes/Inicio';
+
+import Login from './componentes/Login';
+import Perfil from './componentes/Perfil';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Inicio />
-      </header>
-    </div>
+
+  const [pantalla, setPantalla] = useState(
+    localStorage.getItem("id_usuario") ? "perfil" : "login"
   );
+
+  const manejarLoginCorrecto = () => {
+    setPantalla("perfil");
+  };
+
+  const manejarCerrarSesion = () => {
+    localStorage.removeItem("id_usuario");
+    setPantalla("login");
+  };
+
+  const manejarCrearCuenta = () => {
+    console.log("Aquí después conectaremos Registro");
+  };
+
+return (
+  <div className="App">
+
+    <header className="App-header">
+
+      {pantalla === "login" && (
+        <Login
+          onCrearCuenta={manejarCrearCuenta}
+          onLoginCorrecto={manejarLoginCorrecto}
+        />
+      )}
+
+      {pantalla === "perfil" && (
+        <Perfil
+          onCerrarSesion={manejarCerrarSesion}
+        />
+      )}
+
+    </header>
+
+  </div>
+);
 }
 
 export default App;
