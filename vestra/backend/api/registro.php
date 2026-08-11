bf417072f44495dd3e3e7b535f264b93a2a4868d
BelenAbarcaM <belenabarcamonge@gmail.com>
2026-08-11 17:46:50 -0600

    Add files via upload

--- vestra/backend/api/registro.php
+++ vestra/backend/api/registro.php
@@ -9,10 +9,10 @@ include '../models/Usuario.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
 
 
 $nombre = $_POST['name'] ?? '';
